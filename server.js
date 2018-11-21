671b00c0af28243380350f2a824997a718853a7a
WARATEP TUBBOON <waratep_tubboon@icloud.com>
2018-11-21 22:36:59 +0700

    upgrade!

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var dgram = require("dgram");
 var server = dgram.createSocket("udp4");
+const request = require('request')
 
 server.on("error", function (err) {
     console.log("server error:\n" + err.stack);
@@ -8,6 +9,7 @@ server.on("error", function (err) {
 
 server.on("message", function (msg, rinfo) {
     console.log("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port);
+    line_notify(msg);
     var ack = new Buffer("Hello ack");
     server.send(ack, 0, ack.length, rinfo.port, rinfo.address, function(err, bytes) {
       console.log("sent ACK.");
@@ -24,3 +26,25 @@ server.bind({
     port: 7000,
     exclusive: true
   });
+
+function line_notify(msg) {
+    request({
+        method: 'POST',
+        uri: 'https://notify-api.line.me/api/notify',
+        header: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        auth: {
+          bearer: 'dvR6TeBMQlqxEJDcRANBpTi6KyJlFMEWeEd9nTFLTbH', //token
+        },
+        form: {
+          message: msg, //ข้อความที่จะส่ง
+        },
+      }, (err, httpResponse, body) => {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log(body)
+        }
+      })
+ } 
